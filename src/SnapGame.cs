e8247a6748d81a88e47c3bfaf6cbe9a0757b4ad6
Haydenw37 <haydenwitting@gmail.com>
2015-08-18 15:51:37 +1000

    change text font to Chunkfive and altered colour to white

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -38,16 +38,17 @@ namespace CardGames
 
 			// Draw the top card
 			Card top = myGame.TopCard;
+			SwinGame.LoadFontNamed("GameFont", "Chunkfive.otf", 24);
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.White, 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
